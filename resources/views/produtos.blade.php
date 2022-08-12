d84f49f17e676071952858140a6b0594e2abe49b
Ellen <ellencabral2098@gmail.com>
2022-08-12 16:40:25 -0300

    add components

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,41 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Produtos</title>
-</head>
-<body>
+<x-layouts.app>
     <h1>Produtos</h1>
     @if ($produtos->count()>0)
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Preço</th>
-                <th>Editar</th>
-                <th>Remover</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($produtos as $produto)
-            <tr>
-                <td><a href="/produto/{{ $produto->id }}">{{$produto->id}}</a></td>
-                <td>{{$produto->nome}}</td>
-                <td>{{$produto->preco}}</td>
-                <td><a href="{{ route('edit_produto', $produto->id) }}">✏️</a></td>                
-                <td><a href="{{ route('remove_produto', $produto->id) }}">🗑️</a></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+        <x-table.products :products="$produtos"/>
     @else
     <p>Produtos não encontrados!</p>
     @endif
-    
-    <br/>
-    <a href="/produto">+ Inserir Produto</a>
-</body>
-</html>
+</x-layouts.app>
